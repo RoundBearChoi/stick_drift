0a1d0ceb8ca891eeaf3aee26aa5e639fdc26c6d9
Roundbeargames Choi <roundbeargames@gmail.com>
2026-08-14 21:14:07 +0900

    loading run sprite

--- src/resources.ts
+++ src/resources.ts
@@ -1,10 +1,12 @@
 import { TerminalLoader } from './terminal_loader';
 import { debugFontSource } from './debug_font';
 /** plugin-aesprite loads + parses the files and convert them to excalibur's graphics objects */
-import { AsepriteResource } from '@excaliburjs/plugin-aseprite';
+import { AsepriteResource, AsepriteSpriteSheet } from '@excaliburjs/plugin-aseprite';
 
 export const Resources = {
   stick_runner_idle: new AsepriteResource('./res/sprites/stick_runner_idle.aseprite'),
+  stick_runner_run: new AsepriteResource('./res/sprites/stick_runner_run.aesprite'),
+
   work_in_progress: new AsepriteResource('./res/sprites/work_in_progress.aseprite'),
 } as const;
 
