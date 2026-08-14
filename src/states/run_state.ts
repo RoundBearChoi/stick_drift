e3e7448d928ddc462688f56a4adf4d517cfc08b1
Roundbeargames Choi <roundbeargames@gmail.com>
2026-08-14 19:28:41 +0900

    add RunState

---
+++ src/states/run_state.ts
@@ -0,0 +1,24 @@
+import { InputAction, InputInterpreter } from '../input_interpreter';
+import { StickRunner } from '../stick_runner';
+import { RunnerState } from './runner_state';
+import { IdleState } from './idle_state';
+import { JumpState } from './jump_state';
+
+export class RunState implements RunnerState {
+  readonly name = 'run';
+
+  enter(runner: StickRunner): void {
+    runner.playAnimationForState(this.name);
+  }
+
+  update(runner: StickRunner, input: InputInterpreter): void {
+    if (input.wasPressed(InputAction.JUMP)) {
+      runner.setState(new JumpState());
+      return;
+    }
+
+    if (!input.isHeld(InputAction.MOVE_LEFT) && !input.isHeld(InputAction.MOVE_RIGHT)) {
+      runner.setState(new IdleState());
+    }
+  }
+}
