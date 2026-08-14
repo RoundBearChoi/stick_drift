7f0d3883881398da8274adf416dcd953a457bafe
Roundbeargames Choi <roundbeargames@gmail.com>
2026-08-14 11:42:03 +0900

    Fix stick runner animation freeze on scene re-entry (re-register on activate)

--- src/test_scene_1.ts
+++ src/test_scene_1.ts
@@ -59,12 +59,14 @@ export class TestScene1 extends Scene<GameContext> {
       }
     }
 
-    // add runner
+    // create runner once
     if (!this._stick_runner) {
       this._stick_runner = createStickRunner(this.engine);
-      this._stick_runner.register(this._game_ctx);
       this.add(this._stick_runner);
     }
+
+    // always re-register when (re)entering the scene so fixed updates resume
+    this._stick_runner.register(this._game_ctx);
   }
 
   onPostUpdate(engine: Engine, elapsed: number): void {
