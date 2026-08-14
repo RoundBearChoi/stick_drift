cda773404ab8d145b4305b5a0372d5bdea57a975
Roundbeargames Choi <roundbeargames@gmail.com>
2026-08-14 19:29:41 +0900

    wire StickRunnerController into GameplayTestScene1

--- src/gameplay_test_scene_1.ts
+++ src/gameplay_test_scene_1.ts
@@ -5,11 +5,13 @@ import {
 } from 'excalibur';
 import { GameContext } from './game_context';
 import { StickRunner } from './stick_runner';
+import { StickRunnerController } from './stick_runner_controller';
 import { createStickRunner } from './stick_runner_creator';
 
 export class GameplayTestScene1 extends Scene<GameContext> {
   private _game_ctx!: GameContext;
   private _stick_runner?: StickRunner;
+  private _controller?: StickRunnerController;
 
   onInitialize(_engine: Engine): void {}
 
@@ -22,9 +24,19 @@ export class GameplayTestScene1 extends Scene<GameContext> {
       this.add(this._stick_runner);
     }
 
+    if (!this._controller) {
+      this._controller = new StickRunnerController(
+        this._stick_runner,
+        this._game_ctx
+      );
+    }
+
     // deterministic start every time we enter the scene
     this._stick_runner.reset();
+
+    // both need to be registered so they receive fixedUpdate
     this._stick_runner.register(this._game_ctx);
+    this._controller.register();
   }
 
   onPostUpdate(engine: Engine, elapsed: number): void {
@@ -32,10 +44,13 @@ export class GameplayTestScene1 extends Scene<GameContext> {
   }
 
   onDeactivate(): void {
-    // unregister so the runner stops being ticked after leaving the scene.
-    // we reuse the same runner instance when we return instead of creating a new one.
+    // unregister so they stop being ticked after leaving the scene.
+    // we reuse the same instances when we return instead of creating new ones.
     if (this._stick_runner) {
       this._stick_runner.unregister(this._game_ctx);
+    }
+    if (this._controller) {
+      this._controller.unregister();
     }
   }
 }
