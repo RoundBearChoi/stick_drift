e88245f4fa77da0a76feb029e03884f4fcb84ba6
Roundbeargames Choi <roundbeargames@gmail.com>
2026-08-16 13:49:16 +0900

    RunnerJump accepts RunnerContext (placeholder for now)

--- src/states/runner_jump.ts
+++ src/states/runner_jump.ts
@@ -1,14 +1,23 @@
 import { InputInterpreter } from '../input_interpreter';
 import { StickRunner } from '../stick_runner';
+import { RunnerContext } from '../runner_context';
 import { RunnerState, RunnerStateName } from './runner_state';
 
 export class RunnerJump implements RunnerState {
   readonly state_name = RunnerStateName.JUMP;
 
-  onEnter(runner: StickRunner): void {
-    runner.playAnimationForState(this.state_name);
+  onEnter(runner: StickRunner, runnerCtx: RunnerContext): void {
+    // for now jump reuses idle animation + idle tick rate
+    runner.playAnimationForState(
+      this.state_name,
+      runnerCtx.idle_animation_tick_per_frames
+    );
   }
 
-  onFixedUpdate(_runner: StickRunner, _input: InputInterpreter): void {
+  onFixedUpdate(
+    _runner: StickRunner,
+    _input: InputInterpreter,
+    _runnerCtx: RunnerContext
+  ): void {
   }
 }
