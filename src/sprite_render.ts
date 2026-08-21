e76295ede59d9c8872d77036025bf661cbb13dd8
Roundbeargames Choi <roundbeargames@gmail.com>
2026-08-21 22:08:07 +0900

    no offset for now

--- src/sprite_render.ts
+++ src/sprite_render.ts
@@ -4,7 +4,10 @@ import { Actor, Vector, vec } from 'excalibur';
  * render only 1px offset for all sprite actors (bricks, runner, etc).
  * this does not (should not) affect world position, anchors, solid grid, or colliders, etc.
  */
-export const SPRITE_RENDER_OFFSET: Vector = vec(0, -1);
+//export const SPRITE_RENDER_OFFSET: Vector = vec(0, -1);
+
+// no offset for now
+export const SPRITE_RENDER_OFFSET: Vector = vec(0, 0);
 
 export function applySpriteRenderOffset(actor: Actor): void {
   actor.graphics.offset = SPRITE_RENDER_OFFSET;
