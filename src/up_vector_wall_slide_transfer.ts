05d295f3455d735b49f87a56921a22cbb640ae19
roundbeargames <roundbeargames@gmail.com>
2026-09-07 21:40:43 +0900

    up vector adjust

--- src/up_vector_wall_slide_transfer.ts
+++ src/up_vector_wall_slide_transfer.ts
@@ -3,7 +3,7 @@ export function transferUpVectorAcrossWallSlide(up_vector: number): number {
   if (up_vector >= 12) return 10;
 
   switch (up_vector) {
-    case 11: return 9;
+    case 11: return 10;
     case 10: return 9;
     case 9: return 8;
     case 8: return 7;
@@ -11,9 +11,9 @@ export function transferUpVectorAcrossWallSlide(up_vector: number): number {
     case 6: return 5;
     case 5: return 4;
     case 4: return 3;
-    case 3: return 3;
-    case 2: return 2;
-    case 1: return 1;
+    case 3: return 2;
+    case 2: return 1;
+    case 1: return 0;
     default: return 0;
   }
 }
